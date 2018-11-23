bcb12ff2689577bfe20fb8b6c0429a8cbe7208e1
wangqiang <377900058@qq.com>
2018-11-23 14:06:27 +0800

    增加actuator

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,7 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,8 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World!") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World!") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, name));
     }
 }
